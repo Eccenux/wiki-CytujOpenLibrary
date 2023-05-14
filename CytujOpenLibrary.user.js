3d0e9ce60541902e2385776123bf96d60ed15c8d
Nux <eccenux+git@gmail.com>
2023-05-15 01:39:02 +0200

    create object

--- CytujOpenLibrary.user.js
+++ CytujOpenLibrary.user.js
@@ -12,6 +12,20 @@
 
 // fetch
 var CytujOpenLibrary = class {
+	async load() {
+		this.rdf = await cytuj.loadCurrentRdf();
+		this.json = await cytuj.loadCurrentJson();
+		let quote = {
+			authors: this.readAuthors(),
+			title: this.json.title,
+			isbn: this.readIsbn(),
+			publisher: this.firstArray(this.json.publishers),
+			date: this.json.publish_date,
+			lang: this.readLangs(),
+		};
+		console.log(quote);
+	}
+
 	/** Load RDF XML as a promise. */
 	loadCurrentRdf() {
 		let url = document.querySelector('#historyTools a[href$=rdf]').href;
@@ -25,18 +39,48 @@ var CytujOpenLibrary = class {
 		return fetch(url)
 			.then(response => response.json())
 	}
-	async load() {
-		this.rdf = await cytuj.loadCurrentRdf();
-		this.json = await cytuj.loadCurrentJson();
-    console.log(this.json)
-	}
+	/** Read authors as an array. */
 	readAuthors() {
-		const authorList = xmlDoc.getElementsByTagName("bibo:authorList")[0];
+		const authorList = this.rdf.getElementsByTagName("bibo:authorList")[0];
 		const authorNames = authorList.getElementsByTagName("foaf:name");
-		// Wyświetlenie imion autorów
-		for (let i = 0; i < authorNames.length; i++) {
-			console.log(authorNames[i].textContent);
+		return authorNames.map(el=>el.textContent);
+	}
+	/** Read ISBN 13 with 10 fallback. */
+	readIsbn() {
+		let isbn = this.json.isbn_13;
+		if (typeof isbn !== 'string') {
+			isbn = this.json.isbn_10;
+			if (typeof isbn !== 'string') {
+				isbn = this.json.isbn;
+			}
 		}
+		return isbn;
+	}
+	/** Read langs as an array. */
+	readLangs() {
+		if (Array.isArray(this.json.languages) && this.json.languages.length) {
+			return this.json.languages.map(l=>l.key.replace('/languages/', '')).map(k=>{
+				if (k === 'eng') {
+					return 'en';
+				}
+				return k;
+			});
+		}
+		return [];
+	}
+	/** Safely flatten the array. */
+	flatArray(arr) {
+		if (!Array.isArray(arr) || !arr.length) {
+			return '';
+		}
+		arr.join(', ');
+	}
+	/** Safe 1st. */
+	firstArray(arr) {
+		if (!Array.isArray(arr) || !arr.length) {
+			return '';
+		}
+		return arr[0];
 	}
 }
 
