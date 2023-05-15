1546c87c2f80c8ebb0c793289448d6620abff2ff
Nux <eccenux+git@gmail.com>
2023-05-15 10:42:28 +0200

    open dialog

--- CytujOpenLibrary.user.js
+++ CytujOpenLibrary.user.js
@@ -10,8 +10,11 @@
 // @downloadURL  https://github.com/Eccenux/wiki-CytujOpenLibrary/raw/master/CytujOpenLibrary.user.js
 // ==/UserScript==
 
-// fetch
+/**
+ * Fetch & render.
+ */
 var CytujOpenLibrary = class {
+	/** Fetch (main). */
 	async load() {
 		this.rdf = await cytuj.loadCurrentRdf();
 		this.json = await cytuj.loadCurrentJson();
@@ -27,6 +30,7 @@ var CytujOpenLibrary = class {
 		return quote;
 	}
 
+	/** Render loaded for pl. */
 	renderPl(quote) {
 		let author = this.flatArray(quote.authors, ', ');
 		let publisher = this.firstArray(quote.publishers);
@@ -41,20 +45,20 @@ var CytujOpenLibrary = class {
 		}}`.replace(/\n\s+/g, ' ');
 	}
 
-	/** Load RDF XML as a promise. */
+	/** @private Load RDF XML as a promise. */
 	loadCurrentRdf() {
 		let url = document.querySelector('#historyTools a[href$=rdf]').href;
 		return fetch(url)
 			.then(response => response.text())
 			.then(str => ((new DOMParser()).parseFromString(str, 'text/xml')))
 	}
-	/** Load JSON based object as a promise. */
+	/** @private Load JSON based object as a promise. */
 	loadCurrentJson() {
 		let url = document.querySelector('#historyTools a[href$=json]').href;
 		return fetch(url)
 			.then(response => response.json())
 	}
-	/** Read authors as an array. */
+	/** @private Read authors as an array. */
 	readAuthors() {
 		const authorList = this.rdf.getElementsByTagName("bibo:authorList")[0];
 		if (authorList) {
@@ -63,7 +67,7 @@ var CytujOpenLibrary = class {
 		}
 		return [];
 	}
-	/** Read ISBN 13 with 10 fallback. */
+	/** @private Read ISBN 13 with 10 fallback. */
 	readIsbn() {
 		let isbn = '';
 		if (Array.isArray(this.json.isbn_13)) {
@@ -75,7 +79,7 @@ var CytujOpenLibrary = class {
 		}
 		return isbn;
 	}
-	/** Read langs as an array. */
+	/** @private Read langs as an array. */
 	readLangs() {
 		if (Array.isArray(this.json.languages) && this.json.languages.length) {
 			return this.json.languages.map(l=>l.key.replace('/languages/', '')).map(k=>{
@@ -87,14 +91,14 @@ var CytujOpenLibrary = class {
 		}
 		return [];
 	}
-	/** Safely flatten the array. */
+	/** @private Safely flatten the array. */
 	flatArray(arr, separator) {
 		if (!Array.isArray(arr) || !arr.length) {
 			return '';
 		}
 		return arr.join(separator);
 	}
-	/** Safe 1st. */
+	/** @private Safe 1st. */
 	firstArray(arr) {
 		if (!Array.isArray(arr) || !arr.length) {
 			return '';
@@ -103,7 +107,76 @@ var CytujOpenLibrary = class {
 	}
 }
 
+/**
+ * Quote actions.
+ */
+var QuoteButtons = class {
+	/**
+	 * cons.
+	 * @param {CytujOpenLibrary} cytuj 
+	 */
+	constructor(cytuj) {
+		this.q = cytuj;
+	}
+
+	/**
+	 * Get Polish.
+	 * @param {Function} callback 
+	 */
+	getPl(callback) {
+		// TODO: add cashing? (don't load 2nd time?)
+		this.q.load().then((quote)=>{
+			console.log(this.q.renderPl(quote));
+			callback(this.q.renderPl(quote));
+		});
+	}
+
+	/** Open with quote text. */
+	openDialog(text) {
+		let dialog = document.createElement('dialog');
+		dialog.innerHTML = `
+		  <form>
+			<p>
+			  <textarea></textarea>
+			</p>
+			<div>
+			  <button class="dialog-ok" value="default">OK</button>
+			</div>
+		  </form>
+		`;
+		dialog.querySelector('textarea').value = text;
+		dialog.querySelector('.dialog-ok').addEventListener('click', (event) => {
+			event.preventDefault();	// prevent submit
+			dialog.close();
+		});
+		document.body.appendChild(dialog);
+		dialog.showModal();
+	}
+
+	init() {
+		// fix current
+		const enQuote = document.querySelector('#wikilink');
+		enQuote.textContent = 'cite book (en.wikipedia)';
+		enQuote.title = 'Zacytuj to na Angielskiej Wikipedii';
+		
+		const container = document.querySelector('#historyTools');
+		// append pl
+		const el = document.createElement('a');
+		el.textContent = 'Cytuj książkę (pl.wikipedia)';
+		el.title = 'Zacytuj to na Polskiej Wikipedii';
+		el.href = 'javascript:;';
+		el.onclick = () => {
+			this.getPl((text) => {
+				this.openDialog(text);
+			})
+		};
+		container.appendChild(document.createTextNode('•'));
+		container.appendChild(el);
+	}
+}
+
 var cytuj = new CytujOpenLibrary();
-cytuj.load().then((quote)=>{
-	console.log(cytuj.renderPl(quote))
+var buttons = new QuoteButtons(cytuj);
+window.addEventListener('load', () => {
+	buttons.init();
 });
